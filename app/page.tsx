15df99c78314dcaa704871c75223e358670cde84
InfinityScripter <talalaev.misha@gmail.com>
2024-06-08 12:31:50 +0300

    fix : style layout

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,8 @@ export default function Home() {
                 <Humidity />
                 <Visibility />
                 <Pressure />
-                <Mapbox/>
               </div>
+              <Mapbox/>
             </div>
           </div>
         </main>
